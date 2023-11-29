ca43b2516ed6d9f5b3d790f83f5e7e9333389c61
Reza Amya <rezaamya@gmail.com>
2023-11-29 17:09:17 +0330

    Modify: Add CSS and Tailwind

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,26 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import MainHeader from "@/components/header/main-header";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import MainHeader from '@/components/header/main-header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'ReNa Chefs',
-  description: 'A Full-Stack exercise by RezaAmya@gmail.com using Next.js, GraphQL, SocketIO, NestJS, RabbitMQ, Docker, Microservice Architecture (Saga Orchestration)',
-}
+  description:
+    'A Full-Stack exercise by RezaAmya@gmail.com using Next.js, GraphQL, SocketIO, NestJS, RabbitMQ, Docker, Microservice Architecture (Saga Orchestration)',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <MainHeader></MainHeader>
-        {children}
+        <MainHeader />
+        <div className="container mx-auto">{children}</div>
       </body>
     </html>
-  )
+  );
 }
--- frontend/components/header/main-header.tsx
+++ frontend/components/header/main-header.tsx
@@ -1,35 +1,34 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function MainHeader() {
-    return (
-        <>
-            <header>
-                <div>ReNa Chefs</div>
-                <hr></hr>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link href={'/recipes'}>Recipes</Link>
-                        </li>
-                        <li>
-                            <Link href={'/recipes/add'}>Add recipe</Link>
-                        </li>
-                        <li>
-                            <Link href={'/ingredients'}>Ingredients</Link>
-                        </li>
-                        <li>
-                            <Link href={'/ingredients/add'}>Add ingredient</Link>
-                        </li>
-                        <li>
-                            <Link href={'/login'}>Login</Link>
-                        </li>
-                        <li>
-                            Logout
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <hr></hr>
-        </>
-    )
+  return (
+    <>
+      <header>
+        <div className="flex flex-row bg-indigo-500 text-center pt-8 pb-8">
+          <div className="basis-full text-white text-4xl">ReNa Chefs</div>
+        </div>
+        <nav className="flex pt-2 pb-2 w-screen">
+          <ul className="flex-1 flex justify-center mr-auto">
+            <li className="mx-2">
+              <Link href={'/recipes'}>Recipes</Link>
+            </li>
+            <li className="mx-2">
+              <Link href={'/recipes/add'}>Add recipe</Link>
+            </li>
+            <li className="mx-2">
+              <Link href={'/ingredients'}>Ingredients</Link>
+            </li>
+            <li className="mx-2">
+              <Link href={'/ingredients/add'}>Add ingredient</Link>
+            </li>
+            <li className="mx-2">
+              <Link href={'/login'}>Login</Link>
+            </li>
+            <li className="mx-2">Logout</li>
+          </ul>
+        </nav>
+      </header>
+      <hr className="pb-3" />
+    </>
+  );
 }
